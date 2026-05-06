ce97968979845caa81de875d86cede0e5f1cb030
ahmadabd <ahmadabd13741112@gmail.com>
2026-05-06 17:49:26 +0330

    Add locast

---
+++ README.md
@@ -0,0 +1,3 @@
+## Locast
+>> ./.venv/bin/locust --headless -u 10 -r 2 --run-time 1m --host http://localhost
+>> ./.venv/bin/locust
---
+++ locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task, between
+
+class APIUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def test_root(self):
+        self.client.get("/")
+
+    @task(1)
+    def check_health(self):
+        self.client.get("/health")
